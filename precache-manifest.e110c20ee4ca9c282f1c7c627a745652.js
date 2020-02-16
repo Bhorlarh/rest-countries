663032ecdc38677363138517c486a3730015857e
Bhorlarh <48389765+Bhorlarh@users.noreply.github.com>
2020-02-16 15:17:12 +0100

    Updates

---
+++ precache-manifest.e110c20ee4ca9c282f1c7c627a745652.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b20397856f2194fd5a8083c496bf560",
+    "url": "/rest-countries/index.html"
+  },
+  {
+    "revision": "776881fee505d0f05ef9",
+    "url": "/rest-countries/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8f9ed58a7c3226dfd333",
+    "url": "/rest-countries/static/css/main.7995aff7.chunk.css"
+  },
+  {
+    "revision": "776881fee505d0f05ef9",
+    "url": "/rest-countries/static/js/2.b1e29a63.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rest-countries/static/js/2.b1e29a63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f9ed58a7c3226dfd333",
+    "url": "/rest-countries/static/js/main.69a73447.chunk.js"
+  },
+  {
+    "revision": "1b11e2c9aeb5e9d7912f",
+    "url": "/rest-countries/static/js/runtime-main.23d67e19.js"
+  },
+  {
+    "revision": "17c9ab99541860d01af25ecce019a7dc",
+    "url": "/rest-countries/static/media/moon.17c9ab99.png"
+  }
+]);
